8865f54581c35a4a5bf7cb8a65d75e9b96824743
seed <seed@tor.atm>
2013-08-05 07:37:29 +0000

    update

--- lib/bitcoin/version.rb
+++ lib/bitcoin/version.rb
@@ -2,7 +2,7 @@ module Bitcoin
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 10
+    PATCH = 11
     REL = nil
     STRING = REL ? [MAJOR, MINOR, PATCH, REL].join('.') : [MAJOR, MINOR, PATCH].join('.')
   end
